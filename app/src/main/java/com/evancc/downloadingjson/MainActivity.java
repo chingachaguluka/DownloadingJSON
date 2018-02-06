3fb0eb1a35c11daeb562344a84c6b9ee5dd4d164
Chinga Chaguluka <chinga@evancc.com>
2018-02-06 23:15:31 +0200

    Application completed

--- app/src/main/java/com/evancc/downloadingjson/MainActivity.java
+++ app/src/main/java/com/evancc/downloadingjson/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -15,6 +18,11 @@ import java.net.URL;
 import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText = null;
+    TextView mainTextView = null;
+    TextView weatherTextView = null;
+    String weatherURL = "";
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -45,35 +53,50 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onButtonClick(View view) {
+        //Clearing previous results
+        mainTextView.setText("-");
+        weatherTextView.setText("-");
 
+        if(editText.getText().length() > 0) {
+            weatherURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + editText.getText() + "&APPID=0f39e5392290578788165eeac5ae55bc";
+
+            DownloadTask task = new DownloadTask();
+            try {
+                String weather = task.execute(weatherURL).get();
+                JSONObject weatherObject = new JSONObject(weather);
+                //Iterator keys = weatherObject.keys();
+
+                String weatherInfo = weatherObject.getString("list");
+                Log.i("Weather Info", weatherInfo);
+                JSONArray weatherArray = new JSONArray(weatherInfo);
+                JSONObject currentWeather = weatherArray.getJSONObject(1);
+                weatherInfo = currentWeather.getString("weather");
+                weatherArray = new JSONArray(weatherInfo);
+                currentWeather = weatherArray.getJSONObject(0);
+
+                //Log.i("Main", currentWeather.getString("main"));
+                //Log.i("Description", currentWeather.getString("description"));
+                mainTextView.setText(currentWeather.getString("main"));
+                weatherTextView.setText(currentWeather.getString("description"));
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DownloadTask task = new DownloadTask();
-        try {
-            String weather = task.execute("http://api.openweathermap.org/data/2.5/forecast?q=London,us&APPID=0f39e5392290578788165eeac5ae55bc").get();
-            JSONObject weatherObject = new JSONObject(weather);
-            //Iterator keys = weatherObject.keys();
-
-            String weatherInfo = weatherObject.getString("list");
-            Log.i("Weather Info", weatherInfo);
-            JSONArray weatherArray = new JSONArray(weatherInfo);
-            JSONObject currentWeather = weatherArray.getJSONObject(1);
-            weatherInfo = currentWeather.getString("weather");
-            weatherArray = new JSONArray(weatherInfo);
-            currentWeather = weatherArray.getJSONObject(0);
-
-            Log.i("Main", currentWeather.getString("main"));
-            Log.i("Description", currentWeather.getString("description"));
+        editText = (EditText) findViewById(R.id.editText);
+        mainTextView = (TextView) findViewById(R.id.mainTextView);
+        weatherTextView = (TextView) findViewById(R.id.weatherTextView);
 
 
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 }
